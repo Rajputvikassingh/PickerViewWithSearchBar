607c85c6ec7b2ae76d020eb2041295fafca59541
Vikas Singh <mac12@MAC12s-Mac-Pro.local>
2014-05-26 14:55:46 +0530

    Initial Commit

---
+++ PickerViewWithSearchBar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PickerViewWithSearchBar
+//
+//  Created by Vikas Singh on 26/05/14.
+//  Copyright (c) 2014 Vikas Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ PickerViewWithSearchBarTests/PickerViewWithSearchBarTests.m
@@ -0,0 +1,34 @@
+//
+//  PickerViewWithSearchBarTests.m
+//  PickerViewWithSearchBarTests
+//
+//  Created by Vikas Singh on 26/05/14.
+//  Copyright (c) 2014 Vikas Singh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PickerViewWithSearchBarTests : XCTestCase
+
+@end
+
+@implementation PickerViewWithSearchBarTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
